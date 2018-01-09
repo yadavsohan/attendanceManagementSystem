bc8ae10f985661c78ca3a9ac73e214a9587e087b
sohan yadav <32355277+yadavsohan@users.noreply.github.com>
2018-01-10 02:33:47 +0530

    i have some chenged in teacher class

--- core/src/main/java/hu.uni.miskolc.iit.sweng.attendancemanagementsystem/core.model/Teachers.java
+++ core/src/main/java/hu.uni.miskolc.iit.sweng.attendancemanagementsystem/core.model/Teachers.java
@@ -2,7 +2,27 @@ package core.model;
 
 public class Teachers {
 
-    public static void main(String []args) {
-        System.out.println("Hello World");
+    private int _Teacher_id;
+    private String _Teacher_name;
+
+    public Teachers( int Teacher_id,String Teacher_name ){
+        this._Teacher_id = Teacher_id;
+        this._Teacher_name = Teacher_name;
+    }
+
+    public int get_Teacher_id() {
+        return _Teacher_id;
+    }
+
+    public String get_Teacher_name() {
+        return _Teacher_name;
+    }
+
+    public void set_Teacher_id(int _Teacher_id) {
+        this._Teacher_id = _Teacher_id;
+    }
+
+    public void set_Teacher_name(String _Teacher_name) {
+        this._Teacher_name = _Teacher_name;
     }
 }
