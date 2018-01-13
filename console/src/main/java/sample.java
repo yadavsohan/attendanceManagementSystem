8314a1b9bcb96c16f53e7749d004aa1df4d2cc53
sohan yadav <32355277+yadavsohan@users.noreply.github.com>
2018-01-14 01:07:48 +0530

    initialize the project

---
+++ console/src/main/java/sample.java
@@ -0,0 +1,20 @@
+package console.sample;
+interface A
+{
+    public void aaa();
+}
+interface B
+{
+    public void aaa();
+}
+class Central implements A,B
+{
+    public void aaa()
+    {
+        //Any Code here
+    }
+    public static void main(String args[])
+    {
+        //Statements
+    }
+}
