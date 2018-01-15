750c439d5ea17ff22436d41daa3173b4c206ccf1
sohan yadav <32355277+yadavsohan@users.noreply.github.com>
2018-01-16 03:34:54 +0530

    correct pacakages names

---
+++ services/src/main/java/hu/uni/miskolc/iit/sweng/attendanceManagementSystem/services/App.java
@@ -0,0 +1,8 @@
+package hu.uni.miskolc.iit.sweng.attendanceManagementSystem.services;
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
